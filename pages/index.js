16125526b7c9854e1781a31673ec60003d743cc2
EduardoWeschenfelder <eduardoweschenfelder@alunos.utfpr.edu.br>
2021-03-29 18:26:55 -0300

    First commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { useThrottleButton } from "./components/exemplo";
+
+export default function Home() {
+  const { setNextClick } = useThrottleButton(() => {
+    console.log("Evento disparao");
+  });
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setNextClick(2000);
+        }}
+      >
+        Evento 1
+      </button>
+      <button
+        onClick={() => {
+          setNextClick(3000);
+        }}
+      >
+        Evento 2
+      </button>
+    </>
+  );
+}
